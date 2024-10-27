761b036439f52835c9c61c3d3627a29e8f0b0246
nondairyneutrino <chapmann19@gmail.com>
2024-10-27 16:24:20 -0700

    change initial charge configuration to yield more interesting equipotentials

--- computation/examples/equipotentials.jl
+++ computation/examples/equipotentials.jl
@@ -7,6 +7,7 @@ particleTuple = (
     Particle(charge = 2, position = 1 * [cos(1 * 2pi/3), sin(1 * 2pi/3)]),
     Particle(charge = 2, position = 1 * [cos(2 * 2pi/3), sin(2 * 2pi/3)]),
     Particle(charge = -1,position = [0, 0]),
+    Particle(charge = 10, position = [2,0])
 )
 
 drawEquipotentials(particleTuple)
